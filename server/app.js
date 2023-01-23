752ef41863cf9d54bb761e48b6fbce0b0f77c073
hilma <elyasaf11@gmail.com>
2023-01-23 16:30:16 +0200

    mail

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ var cors = require('cors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var app = express();
-
+var nodemailer = require('nodemailer');
 
 app.use(cors());
 app.use(logger('dev'));
@@ -19,5 +19,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'elyasaf11@gmail.com',
+        pass: 'yfkpuiockfqgykvr'
+    }
+});
+
+var mailOptions = {
+    from: 'elyasaf11@gmail.com',
+    to: 'yotam.hanoch@gmail.com',
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Email sent: ' + info.response);
+    }
+});
+
+
+
 
 module.exports = app;
