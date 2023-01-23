a2814239297f0881122e3e01d40f9cf26e57ae98
hilma <elyasaf11@gmail.com>
2023-01-23 08:53:18 +0200

    php

---
+++ client/public/subscriberform.php
@@ -0,0 +1,16 @@
+<?php
+
+
+if($_POST["message"]) {
+
+
+mail("elyasaf11@gmail.address", "Here is the subject line",
+
+
+$_POST["insert your message here"]. "From: elyasaf11@gmail.address");
+
+
+}
+
+
+?>
