0b522dd1484af4bf64861994758809bcbf0cb388
hilma <yotam.hanoch@gmail.com>
2023-01-24 15:53:40 +0200

    change-password

--- client/src/components/ChangePassword.js
+++ client/src/components/ChangePassword.js
@@ -1,17 +1,28 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function ChangePassword() {
     const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const navigate = useNavigate();
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await fetch(`http://localhost:8080/users/password`, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-                password: password
-            })
-        });
-        setPassword("");
+        const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,20}$/;
+        if(passwordRegex.test(password)){
+            const response = await fetch(`http://localhost:8080/users/changePassword`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    password: password,
+                    email: email
+                })
+            });
+            const data = await response.json();
+            if (data) {
+                console.log(data);
+               navigate('/');
+            }
+        }
     }
 
     return ( 
@@ -22,6 +33,14 @@ function ChangePassword() {
                         <div className="formbg-inner padding-horizontal--48">
                             <span className="padding-bottom--15">Enter your new password</span>
                             <form id="stripe-login">
+                            <div className="field padding-bottom--24">
+                                    <label htmlFor="email">Email</label>
+                                    <input type="email" name="email" value={email}
+                                        onChange={(e) => {
+                                            setEmail(e.target.value);
+                                            console.log(email);
+                                        }} />
+                                </div>
                                 <div className="field padding-bottom--24">
                                     <label htmlFor="email">New password</label>
                                     <input type="password" name="password" value={password}
@@ -30,7 +49,7 @@ function ChangePassword() {
                                         }} />
                                 </div>
                                 <div className="field padding-bottom--24">
-                                    <button onClick={handleSubmit}>Send</button>
+                                    <button onClick={handleSubmit}>Change Password</button>
                                 </div>
                             </form>
                         </div>
