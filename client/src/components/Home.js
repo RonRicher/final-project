80b486b5cdc125da0b39a6dff0081e8cfd93c5eb
hilma <elyasaf11@gmail.com>
2023-01-28 11:54:22 +0200

    gettingBack

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,12 @@
+import NavBar from "./NavBar";
+
 function Home() {
-    return ( 
-        <h1>Home</h1>
-     );
+    return (
+        <>
+            <NavBar />
+            <h1>Home</h1>
+        </>
+    );
 }
 
 export default Home;
---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,31 @@
+import Cookies from "js-cookie";
+import React, { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+
+
+function NavBar() {
+
+    const [userId, setUserId] = useState("");
+
+    useEffect(() => {
+        setUserId(Cookies.get("userId"));
+    });
+
+    function logOut() {
+        Cookies.remove("userId");
+    }
+
+    return (
+        <header id="navBar">
+            <div>
+                <NavLink to="/Home">Home</NavLink>
+                {true ? <NavLink to="/Login">LogIn</NavLink> :
+                    <NavLink onClick={logOut} id="logOutButton" to="/">
+                        Logout
+                    </NavLink>}
+            </div>
+        </header>
+    );
+}
+
+export default NavBar;
