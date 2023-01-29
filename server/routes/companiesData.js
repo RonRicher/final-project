79d44709b9579e76abf20dae139fb6b125242869
hilma <elyasaf11@gmail.com>
2023-01-29 17:31:49 +0200

    dataRoute

--- server/routes/companiesData.js
+++ server/routes/companiesData.js
@@ -16,8 +16,45 @@ router.get('/hotels', async function (req, res) {
         orderBy: [],
         join: []
     });
-    console.log(data);
-    res.send(JSON.stringify(data));
+    let arr = [];
+    data.forEach(hotel => {
+        arr.push({
+            name: hotel.hotel_name,
+            location: hotel.location,
+            roomsLeft: hotel.rooms_left,
+            price: hotel.price
+        });
+    });
+    console.log(arr);
+    res.send(JSON.stringify(arr));
+
+});
+
+
+router.get('/flights', async function (req, res) {
+    const data = await createSQLQuery.sqlSelect({
+        distinct: false,
+        columns: ['*'],
+        tableName: "flight",
+        where: ``,
+        orderBy: [],
+        join: []
+    });
+    let arr = [];
+    flight_id, airline, start_location, destination, flight_date, departure, arriving, price
+
+    data.forEach(flight => {
+        arr.push({
+            airline: flight.airline,
+            startLocation: flight.start_location,
+            destination: flight.destination,
+            flightDate: flight_date,
+            departure: flight.departure,
+            arriving: flight.arriving,
+            price: flight.price
+        });
+    });
+    res.send(JSON.stringify(arr));
 
 });
 module.exports = router;
