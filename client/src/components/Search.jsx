b36b745ea382817286b4e207a78433e08db855cf
hilma <elyasaf11@gmail.com>
2023-01-31 13:01:38 +0200

    cssStart

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -9,13 +9,14 @@ function Search() {
     const [location, setLocation] = useState("");
     const [startDate, setStartDate] = useState("");
     const [endDate, setEndDate] = useState("");
-
+    const [locationFlag, setLocationFlag] = useState(true);
     const [type, setType] = useState("");
     const aa = useRef();
     const navigate = useNavigate();
 
 
     const getLocations = async (value) => {
+
         const response = await fetch(`http://localhost:8080/users/data/search/location?search=${value}`);
         const data = await response.json();
         console.log(data);
@@ -24,49 +25,54 @@ function Search() {
     };
 
     const getSearchDeals = async (location, type) => {
-        navigate('/search/deals', { state: { location, type, startDate, endDate } });
+        console.log(location);
+        // navigate('/search/deals', { state: { location, type, startDate, endDate } });
     };
 
     return (
 
         <div>
-            <h1>Find your next Deal</h1>
-            <h6>Search deals by location or type of trip..</h6>
+            <div className="header-search">
+                <h3 className="main-title">Find your next Deal</h3>
+                <p className="sub-title">Search deals by location or type of trip..</p>
+            </div>
+            <div className="searchComp">
+                <div>
+                    <input className="search-box" placeholder='Where are you going?' type="search" value={locationSearch} onChange={(e) => {
+                        setLocationSearch(e.target.value);
+                        clearTimeout(aa.current);
+                        aa.current = setTimeout(() => getLocations(e.target.value), 500);
+                    }
+                    } />
+                    <ul className="select">
+                        {locationSelect?.map((item) => {
+                            return locationFlag ? <li key={Math.random()} onClick={() => {
+                                setLocation(item);
+                                setLocationFlag(false);
 
-            <input placeholder='Where are you going?' type="search" value={locationSearch} onChange={(e) => {
-                setLocationSearch(e.target.value);
-                clearTimeout(aa.current);
-                aa.current = setTimeout(() => getLocations(e.target.value), 500);
-            }
-            } />
-            <select className="" value={location} onChange={(e) => {
-                console.log('dsadasd');
+                            }}>{item}</li> : null;
+                        })}
+                    </ul>
 
-                setLocation(e.target.value);
-            }}>
-                {['select location', ...locationSelect]?.map((item) => {
-                    return <option key={Math.random()} value={item}>{item}</option>;
-                })}
-            </select>
+                </div>
+                <div className="dates">
 
-            <select value={type} onChange={(e) => {
-                setType(e.target.value);
-            }}>
-                <option value="type">type</option>
-                <option value='romantic'>romantic</option>
-                <option value='nature'>nature</option>
-                <option value='urban'>urban</option>
-                <option value='families'>families</option>
-            </select>
+                    <input className="date" type="date" onChange={(e) => setStartDate(e.target.value)} />
 
-            <label>Start Date:</label>
-            <input type="date" onChange={(e) => setStartDate(e.target.value)} />
 
-            <label>End Date:</label>
-            <input type="date" onChange={(e) => setEndDate(e.target.value)} />
-
-            <button onClick={() => getSearchDeals(location, type)}></button>
-
+                    <input className="date" type="date" onChange={(e) => setEndDate(e.target.value)} />
+                </div>
+                <select value={type} onChange={(e) => {
+                    setType(e.target.value);
+                }}>
+                    <option value="type">type</option>
+                    <option value='romantic'>romantic</option>
+                    <option value='nature'>nature</option>
+                    <option value='urban'>urban</option>
+                    <option value='families'>families</option>
+                </select>
+                <button onClick={() => getSearchDeals(location, type)}>Search</button>
+            </div>
         </div>
 
     );
